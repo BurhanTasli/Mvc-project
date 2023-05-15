d456153748e9f2a01644906639b35af3e22e1174
BurhanTasli <115864915+BurhanTasli@users.noreply.github.com>
2023-05-15 14:10:23 +0300

    Add project files.

---
+++ Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanzHow.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            //veri tabanından bütün blog bilgileri al ve view üzerine gönder.
+            //kendisine gelen blog bilgilerini dinamik içerik üreten View yapısı bunu static Html'e cevirir. 
+            
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            return View();
+        }
+    }
+}
